74c194da7809f4b3b0c062c70c330b6a06e19a1d
Jesse Rego-Martin <jarsenault02@gmail.com>
2021-02-15 18:54:19 -0500

    Set up Actions and Reducers

--- store/actions/action_types.js
+++ store/actions/action_types.js
@@ -1,2 +1,18 @@
 // Holds all the string action types in variables. This allows for easy modification of action types, since 
 // we will only have to change them in this file.
+
+// boilerplate actions
+export const SUCCESS = "SUCCESS";
+export const FAILURE = "FAILURE";
+
+// For updating the authentication state of the user
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+
+// Used to save profile data from Auth0 to the global state
+export const ADD_PROFILE = "ADD_PROFILE";
+export const REMOVE_PROFILE = "REMOVE_PROFILE";
+
+// Used to track changes and submit any user-submitted text from the form
+export const USER_INPUT_CHANGE = "USER_INPUT_CHANGE";
+export const USER_INPUT_SUBMIT = "USER_INPUT_SUBMIT";
--- store/reducers/form_reducer.js
+++ store/reducers/form_reducer.js
@@ -1,1 +1,32 @@
 // Reducer that reads and updates state properties related to forms
+
+import { __RouterContext } from 'react-router';
+import * as ACTION_TYPES from '../actions/action_types';
+
+export const initialState = {
+    user_textChange: '',
+    user_textSubmit: ''
+}
+
+export const FormReducer = (state, action) => {
+    switch(action.type) {
+
+        // track changes to input element
+        case ACTION_TYPES.USER_INPUT_CHANGE:
+            return {
+                ...state,
+                user_textChange: action.payload
+            }
+
+        // adds submitted form to the global state
+        case ACTION_TYPES.USER_INPUT_SUBMIT:
+            return {
+                ...state,
+                user_textSubmit: action.payload
+            }
+
+        // otherwise, error out
+        default:
+            throw new Error();
+    }
+}
